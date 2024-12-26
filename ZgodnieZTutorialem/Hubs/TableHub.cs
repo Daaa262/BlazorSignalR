0383d86a8b16ed445e23b4fb301d66793217bc5b
Damia <Damia@DESKTOP-DFKG97C>
2024-12-26 08:41:02 +0100

    Dodaj pliki projektów.

---
+++ ZgodnieZTutorialem/Hubs/TableHub.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using ZgodnieZTutorialem.Models;
+
+namespace ZgodnieZTutorialem.Hubs;
+
+public class TableHub : Hub
+{
+    private static List<Table> table = new();
+
+    public async Task AddTable(string tableName, int playerCount, int chipCount)
+    {
+        for(int i = 0; i < table.Count; i++)
+        {
+            if (table[i].TableName == tableName)
+                return;
+        }
+        //table.Add(new Table(tableName, playerCount, chipCount ));
+        table.Add(new Table(Context.ConnectionId, playerCount, chipCount));
+        await Clients.All.SendAsync("ReceiveNewTable", tableName, playerCount, chipCount);
+    }
+
+    public async Task GetTables()
+    {
+        foreach (var tab in table)
+        {
+            await Clients.Caller.SendAsync("ReceiveNewTable", tab.TableName, tab.PlayerCount, tab.ChipCount);
+        }
+    }
+    public async Task CheckIfPlayerOnTable()
+    {
+        await Clients.Caller.SendAsync("GoToLobby");
+        string ID = Context.ConnectionId;
+        foreach(var tab in table)
+        {
+            for (int i = 0; i < tab.PlayerCount; i++)
+            {
+                if (tab.playerID[i] == ID)
+                {
+                    await Clients.Caller.SendAsync("GoToGame", tab.TableName);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ ZgodnieZTutorialem/Program.cs
@@ -0,0 +1,43 @@
+using ZgodnieZTutorialem.Client.Pages;
+using ZgodnieZTutorialem.Components;
+using Microsoft.AspNetCore.ResponseCompression;
+using ZgodnieZTutorialem.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSignalR();
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        ["application/octet-stream"]);
+});
+
+var app = builder.Build();
+app.UseResponseCompression();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(ZgodnieZTutorialem.Client._Imports).Assembly);
+
+app.MapHub<TableHub>("/tablehub");
+app.Run();
